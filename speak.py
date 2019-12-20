cc18d292cabeba10307c3457725e31675550406d
zaryab2000 <zaryabafser2000@gmail.com>
2019-12-20 16:49:41 +0530

    Initial Commits

---
+++ speak.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import os
+from gtts import gTTS
+
+def tts(text,lang):
+    tts = gTTS(text=text,lang=lang)
+    tts.save("current_audio.mp3")
+    os.system("mpg321 current_audio.mp3")
